a175d76fa334f892fda95c7f84c08c5ad36fd179
Aaron Blum <blumaa@gmail.com>
2020-02-18 14:35:13 +0100

    added book form

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 const app = express();
+
+// allow cross-origin requests
+
+app.use(cors())
 
 // Connect to mlab db
 
