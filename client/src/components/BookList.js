6c5288c9c589a8f985f12b3f52bbd62dc1be233f
Aaron Blum <blumaa@gmail.com>
2020-02-18 13:30:02 +0100

    apollo provider added

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+  render () {
+    return(<div>
+      <ul id='book-list'>
+        <li>Book title</li>
+      </ul>
+    </div>)
+  }
+}
+
+export default BookList;
